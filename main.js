4c799cbfef41123038d840cd65f921b909099010
Munira Rakhmatova <munirarahmatova2004@gmail.com>
2023-09-26 22:31:39 +0500

    Updated

--- main.js
+++ main.js
@@ -81,3 +81,18 @@
     
 //     return result
 // }
+
+
+//Problem 7 - Find the capitals
+// https://www.codewars.com/kata/539ee3b6757843632d00026b/javascript
+
+// var capitals = function (word) {
+//    let newArr = []
+// 	for (let i = 0; i < word.length; i++){
+//        if (word[i] == word[i].toUpperCase()){
+//            newArr.push(i)
+//        }
+//    }
+  
+//    return newArr
+// };
